5f54933a69942497ebc93dc0b10d8f914c45d9f7
paulo <paulo@paulos-Mac-mini.local>
2024-05-23 11:29:25 +0800

    add refresh indicator in user list

--- lib/widgets/user_list.dart
+++ lib/widgets/user_list.dart
@@ -4,6 +4,7 @@ import 'package:users/providers/persons.dart';
 
 import '../model/person.dart';
 import '../screens/user_screen.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class UserList extends ConsumerWidget {
   const UserList(List<Person> persons, {super.key}) : _persons = persons;
@@ -12,26 +13,31 @@ class UserList extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return RefreshIndicator(
-      onRefresh: () async {
-        ref.invalidate(personsProvider);
+    final listView = ListView.builder(
+      itemBuilder: (context, index) {
+        final person = _persons[index];
+        return InkWell(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => UserScreen(person: person)));
+          },
+          child: ListTile(
+            leading: Hero(tag: person, child: const CircleAvatar()),
+            title: Text('${person.firstname} ${person.lastname}'),
+          ),
+        );
       },
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          final person = _persons[index];
-          return InkWell(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => UserScreen(person: person)));
-            },
-            child: ListTile(
-              leading: Hero(tag: person, child: const CircleAvatar()),
-              title: Text('${person.firstname} ${person.lastname}'),
-            ),
-          );
-        },
-        itemCount: _persons.length,
-      ),
+      itemCount: _persons.length,
     );
+
+    if (!kIsWeb) {
+      return RefreshIndicator(
+          onRefresh: () async {
+            ref.invalidate(personsProvider);
+          },
+          child: listView);
+    } else {
+      return listView;
+    }
   }
 }
